5cd7ff2666d924287a4363b7e80f8ae1afbe710d
JackHuynh2001 <9jhuynh@stpatrick.org>
2016-10-12 13:56:44 -0500

    Operating System
    first real project

---
+++ AppObject.js
@@ -0,0 +1,37 @@
+function App(n,m,s){
+
+    //Declare your instance variables here.
+    this.name = n;
+    this.memory = m;
+    this.state = s;
+
+    //Declare the instance function open here.
+    this.open = function(){
+      this.state = "active"
+
+    };
+    //Declare the instance function sleep here.
+this.sleep = function(){
+  this.state = "sleep"
+
+
+}
+
+
+    //Declare the instance function active here.
+this.
+
+
+    //Declare the instance function close here.
+
+
+
+
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+//The order does not matter.

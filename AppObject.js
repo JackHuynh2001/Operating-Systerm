9df5a715f9d01b4a0979cd478ae59b87867a4d7c
JackHuynh2001 <9jhuynh@stpatrick.org>
2016-10-13 13:03:11 -0500

    Finished
    Finished the project

--- AppObject.js
+++ AppObject.js
@@ -12,25 +12,35 @@ function App(n,m,s){
     };
     //Declare the instance function sleep here.
 this.sleep = function(){
-  this.state = "sleep"
+  this.state = "sleep";
+  this.memory/2;
 
 
 }
 
 
     //Declare the instance function active here.
-this.
-
+this.active = function(){
+  if(this.state == "sleep");
+  this.state = "active";
+  this.memory*2;
+}
 
     //Declare the instance function close here.
-
-
-
-
+this.close = function(){
+  if(this.state == "null");
+  this.memory*0;
+  else(this.state == "null")
+}
 }
 
 //Use the constructor to create 4 App objects below.
+var CallofDuty20 = new app(CallofDuty20,10,"active");
+var Pay2Win = new app(Pay2Win,2,"active");
+var TrendingGame = new app(TrendingGame,2,"active");
+var ActualGoodGame = new app(ActualGoodGame,5,"active");
 
+var appList = [CallofDuty20,Pay2Win,TrendingGame,ActualGoodGame]
 
 
 //Declare an array named appList, and place all 4 of your apps in that array.
